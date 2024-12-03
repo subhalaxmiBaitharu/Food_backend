94bb77d141f44191846dac9b26e1b42140305481
Subhalaxmi Baitharu <79510071+subhalaxmiBaitharu@users.noreply.github.com>
2024-12-03 22:24:51 +0530

    Update food.js

--- routes/food.js
+++ routes/food.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../schemas/fooditems.schema");
 const User = require("../schemas/user.schema");
-const { isLoggedIn } = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
 const env = require("dotenv");
 env.config();
 
-router.get('/getAllFoodItems',isLoggedIn, async (req, res) => {
+router.get('/getAllFoodItems', async (req, res) => {
     try {
         const products = await Product.find(); 
         res.status(200).json(products); 
